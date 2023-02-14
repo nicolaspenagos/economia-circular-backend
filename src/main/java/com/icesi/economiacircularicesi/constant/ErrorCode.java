b0c3b7b2e6ff3085a3bfc732acc356ddc9395b36
nicolaspenagos <nicolas.penagosm98@gmail.com>
2023-02-14 16:52:56 -0500

    refactor(CustomException): UserException and UserError have been abstracted to a general CustomException

--- src/main/java/com/icesi/economiacircularicesi/constant/ErrorCode.java
+++ src/main/java/com/icesi/economiacircularicesi/constant/ErrorCode.java
@@ -5,9 +5,14 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum ErrorCode {
+public enum UserErrorCode {
 
-    CODE_E01_INVALID_ARGUMENTS("At least one property is invalid");
+    CODE_U01_IMPOSSIBLE_DATE("Impossible date: the registrations or T&C acceptance date cannot be later than the current date"),
+    CODE_U02_WRONG_DATE_FORMAT("Wrong date format: text cannot be parsed to a date, make sure you are using ISO 8601 yyyy-mm-ddThh:mm:ss format"),
+    CODE_U03_INVALID_EMAIL("Invalid email: the provided email is not a valid email"),
+    CODE_U04_DUPLICATED_EMAIL("Duplicate email: the provided email has already been registered"),
+    CODE_U05_USER_NOT_FOUND("User not found: The user you refer to does not exist");
+
     private String message;
 
 }
--- src/main/java/com/icesi/economiacircularicesi/utils/ErrorExceptionUtils.java
+++ src/main/java/com/icesi/economiacircularicesi/utils/ErrorExceptionUtils.java
@@ -1,13 +1,13 @@
 package com.icesi.economiacircularicesi.utils;
 
 import com.icesi.economiacircularicesi.constant.UserErrorCode;
-import com.icesi.economiacircularicesi.error.exception.UserError.UserError;
-import com.icesi.economiacircularicesi.error.exception.UserError.UserException;
+import com.icesi.economiacircularicesi.error.exception.CustomError.CustomError;
+import com.icesi.economiacircularicesi.error.exception.CustomError.CustomException;
 import org.springframework.http.HttpStatus;
 
 public class UserExceptionUtils {
 
     public static void throwUserException(HttpStatus httpStatus, UserErrorCode code){
-        throw new UserException(httpStatus, new UserError(code, code.getMessage()));
+        throw new CustomException(httpStatus, new CustomError(code, code.getMessage()));
     }
 }
